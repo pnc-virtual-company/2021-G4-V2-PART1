65adf01b97e64f6cb6dbafad3ae40c9721a76b4c
Bunsal.Hul <bunsal.hul@student.passerellesnumeriques.org>
2021-11-27 23:09:04 +0700

    #9 managed user sign in for backend | #10 managed user sign up for backend

--- front/src/main.js
+++ front/src/main.js
@@ -1,4 +1,28 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from "vue-router";
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import Signin from './components/signin_signup/Signin.vue';
+import Signup from './components/signin_signup/Signup.vue';
+
+const router = createRouter({
+    history:createWebHistory(),
+    routes: [
+            {
+                path: "/",
+                component: Signin,
+            },
+            {
+                path: "/signin",
+                component: Signin,
+            },
+            {
+                path: "/signup",
+                component: Signup,
+            },
+        ],
+    });
+
+const app = createApp(App)
+app.use(router);
+app.mount('#app')
