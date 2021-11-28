a7f9dd9284692dfe179f45d61472ce78613a6c3e
saravey2021 <myemail@example.com>
2021-11-28 10:18:02 +0700

    #7 created sign in component

--- front/src/main.js
+++ front/src/main.js
@@ -2,20 +2,20 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from "vue-router";
 import App from './App.vue'
 
-// import Signin from './components/signin_signup/Signin.vue';
+import Signin from './components/signin_signup/Signin.vue';
 import Signup from './components/signin_signup/Signup.vue';
 
 const router = createRouter({
     history:createWebHistory(),
     routes: [
-            // {
-            //     path: "/",
-            //     component: Signin,
-            // },
-            // {
-            //     path: "/signin",
-            //     component: Signin,
-            // },
+            {
+                path: "/",
+                component: Signin,
+            },
+            {
+                path: "/signin",
+                component: Signin,
+            },
             {
                 path: "/signup",
                 component: Signup,
@@ -26,3 +26,4 @@ const router = createRouter({
 const app = createApp(App)
 app.use(router);
 app.mount('#app')
+
