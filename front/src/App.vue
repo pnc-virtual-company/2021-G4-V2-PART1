a35ebd29792594c32e2e612c8b2f4807b090314e
Bunsal.Hul <bunsal.hul@student.passerellesnumeriques.org>
2021-11-29 22:11:36 +0700

    Pull

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,12 +1,28 @@
 <template>
     <section>
-        <router-view></router-view>
+        <router-view @signup="addUser"></router-view>
     </section>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  
+  methods: {
+    addUser(firstname, lastname, email, password, confirm_password){
+      let data = {
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        password: password,
+        password_confirmation: confirm_password
+      }
+      axios.post('http://eventme.com:3000/api/signup', data)
+        .then((res) => {
+          console.log(res.data);
+        });
+    }
+  }
 }
 </script>
 
---
+++ front/src/components/menu/Menu.vue
@@ -0,0 +1,36 @@
+<template>
+    <nav class="navbar  navbar-light border border-dark text-dark light" style='background:#B0F0EC'>
+        <div class='nav justify-content-start w-25'>
+            <h1>Username</h1>
+        </div>
+        <ul class="nav justify-content-end w-75 p-3 ">
+            <li class="nav-item mr-3 ml-2">
+                <a class="nav-link active text-dark font-weight-bold" href="#">My events</a>
+            </li>
+            <li class="nav-item mr-2 ml-2">
+                <a class="nav-link text-dark font-weight-bold" href="#">Find Event</a>
+            </li>
+            <li class="nav-item dropdown font-weight-bold">
+                <a class="nav-link dropdown-toggle text-dark " data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Categories</a>
+                <div class="dropdown-menu mt-4">
+                    <a class="dropdown-item " href="#">#</a>
+                    <a class="dropdown-item" href="#">#</a>
+                    <a class="dropdown-item" href="#">#</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">#</a>
+                </div>
+            </li>
+            <li>
+                <button type="button" class="btn btn-info border border-dark">Sign Out</button>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+
+</style>
