317ff7eacb5afa4835a7e3d468ae2b29f38303cc
sotheanchoun <sothean.choun@student.passerellesnumeriques.org>
2021-12-02 16:53:05 +0700

    #25 category card updated

---
+++ front/src/components/categories/Categories.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <Menu></Menu>
+    <div class="background">
+       <div class="title1">
+              <h2>Welcome To Cambodia</h2>
+          </div>
+    </div>
+    <div class="category_title">
+          <h1>CATEGORY</h1>
+    </div>  
+    <category-card></category-card>
+    <category-card></category-card>
+    <category-card></category-card>
+    <category-card></category-card>
+  </div>
+  
+</template>
+
+<script>
+import Menu from '../menu/Menu.vue'
+export default {
+  components:{ Menu}, 
+};
+</script>
+<style scoped>
+.background{
+  width: 100%;
+  height:550px;
+  background-image: url("../../assets/category_bg.jpg");
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.category_title{
+  background: #000;
+  padding:10px;
+  display: flex;
+  justify-content: center;
+}
+h1{
+  margin-top: 10px;
+  border-radius: 10px;
+  width: 300px;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  background: orange;
+  color: white;
+  font-weight: bold;
+}
+.title1 h2{
+  color: white;
+  font-weight: bold;
+  font-size: 4rem;
+}
+
+</style>
--- front/src/main.js
+++ front/src/main.js
@@ -1,4 +1,51 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from "vue-router";
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import Signin from './components/signin_signup/Signin.vue';
+import Signup from './components/signin_signup/Signup.vue';
+import Myevent from './components/myevent/Myevent.vue';
+import Findevent from './components/findevent/Findevent.vue';
+import Categories from './components/categories/Categories.vue';
+import Category_Card from './components/categories/Category_Card.vue'
+
+const router = createRouter({
+    history:createWebHistory(),
+    routes: [
+            {
+                path: "/",
+                component: Signin,
+            },
+            {
+                path: "/signin",
+                component: Signin,
+            },
+            {
+                path: "/signup",
+                component: Signup,
+            },
+            {
+                path:"/menu",
+                component:Myevent,
+            },
+            {
+                path:"/myevent",
+                component:Myevent
+            },
+            {
+                path:"/findevent",
+                component:Findevent
+            },
+            {
+                path:"/categories",
+                component:Categories
+            }
+        ],
+    });
+
+const app = createApp(App)
+app.use(router);
+app.component('category-card',Category_Card)
+app.mount('#app');
+
+
